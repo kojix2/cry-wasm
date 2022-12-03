e96aaa6c4ab59efcb2ec62d159521d4ed6ba2f81
kojix2 <2xijok@gmail.com>
2022-12-03 22:38:08 +0900

    Add fib64 to example

--- examples/fibonacci.rb
+++ examples/fibonacci.rb
@@ -14,9 +14,24 @@ class Fibonacci
     end
   end
 
+  cry %i[Int64 Int64], :Int64
+  def fib64(n, i)
+    if n <= 1
+      i
+    else
+      fib64(n - 1, i) + fib64(n - 1, i)
+    end
+  end
+
   cry_wasm
 end
 
+a = Fibonacci.new
+
 (1..20).each do |i|
-  puts Fibonacci.new.fib(i)
+  puts a.fib(i)
+end
+
+(1..20).each do |i|
+  puts a.fib(i, 1)
 end
