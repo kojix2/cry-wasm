f52c22d7bc9fd316d6b49734595b399027e27eb2
kojix2 <2xijok@gmail.com>
2022-12-16 20:48:06 +0900

    Update wasmer

--- lib/cry/wasmer.rb
+++ lib/cry/wasmer.rb
@@ -34,40 +34,30 @@ module Cry
     end
 
     def hoge(addr, t2, l, arg)
-      view = get_view(addr, t2)
-      memory_grow(view, l)
-      l.times { |j| view[j] = arg[j] }
+      uint8_view = memory.uint8_view(addr)
+      arg_uint8 = case t2
+                  when 'int8'   then  arg.pack('c*').unpack('C*')
+                  when 'uint8'  then  arg.pack('C*').unpack('C*')
+                  when 'int16'  then  arg.pack('s*').unpack('C*')
+                  when 'uint16' then  arg.pack('S*').unpack('C*')
+                  when 'int32'  then  arg.pack('l*').unpack('C*')
+                  when 'uint32' then  arg.pack('L*').unpack('C*')
+                  when 'int64'  then  arg.pack('q*').unpack('C*')
+                  when 'uint64' then  arg.pack('Q*').unpack('C*')
+                  else raise "unsupported type: #{t2}"
+                  end
+      l.times { |j| uint8_view[j] = arg_uint8[j] }
     end
 
     def get_view(addr, type)
-      offset = get_offset(addr, type)
+      offset = case type
+               when 'int8', 'uint8' then addr / 1
+               when 'int16', 'uint16' then addr / 2
+               when 'int32', 'uint32' then addr / 4
+               # when 'int64', 'uint64' then addr / 8
+               else raise "unsupported type: #{type}"
+               end
       view = memory.public_send("#{type}_view", offset)
-    end
-
-    private
-
-    # FIXME
-    def memory_grow(view, l)
-      loop do
-        flag = false
-        begin
-          view[l]
-          flag = true
-        rescue IndexError
-          memory.grow(1)
-        end
-        break if flag
-      end
-    end
-
-    def get_offset(addr, t)
-      case t
-      when 'int8', 'uint8' then addr / 1
-      when 'int16', 'uint16' then addr / 2
-      when 'int32', 'uint32' then addr / 4
-      # when 'int64', 'uint64' then addr / 8
-      else raise "unsupported type: #{t}"
-      end
     end
   end
 end
