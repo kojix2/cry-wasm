db694c82ce71c604e21627cfd8ea709a82ce52c6
kojix2 <2xijok@gmail.com>
2022-12-18 13:46:21 +0900

    Add wasmtime to benchmark example

--- examples/fib_bench.rb
+++ examples/fib_bench.rb
@@ -1,47 +1,66 @@
 require 'cry/wasm'
+require 'cry/wasmtime'
+require 'cry/wasmer'
 require 'benchmark/plot'
 
-class Fibonacci
+class FibonacciRuby
+  def fib(n)
+    if n <= 2
+      1
+    else
+      fib(n - 1) + fib(n - 2)
+    end
+  end
+end
+
+Cry::Wasm.runtime = Cry::Wasmtime
+
+class FibonacciWasmtime
   extend Cry::Wasm
 
   cry [:Int32], :Int32
   def fib32(n)
-    if n <= 1
+    if n <= 2
       1
     else
       fib32(n - 1) + fib32(n - 2)
     end
   end
 
-  def fib_ruby(n)
-    if n <= 1
+  cry_wasm
+end
+
+Cry::Wasm.runtime = Cry::Wasmer
+
+class FibonacciWasmer
+  extend Cry::Wasm
+
+  cry [:Int32], :Int32
+  def fib32(n)
+    if n <= 2
       1
     else
-      fib_ruby(n - 1) + fib_ruby(n - 2)
+      fib32(n - 1) + fib32(n - 2)
     end
   end
 
   cry_wasm
 end
 
-a = Fibonacci.new
-n = (0...20).to_a
+ruby = FibonacciRuby.new
+wmtm = FibonacciWasmtime.new
+wmer = FibonacciWasmer.new
+
+n = (1...20).to_a
 
 Benchmark.plot(n) do |x|
-  x.report('fib_ruby') do |i|
-    a.fib_ruby(i)
-  end
-
-  x.report('fib_wasm') do |i|
-    a.fib32(i)
-  end
+  x.report('ruby') { |i| ruby.fib(i) }
+  x.report('wasmtime') { |i| wmtm.fib32(i) }
+  x.report('wasmer') { |i| wmer.fib32(i) }
 end
 
 Benchmark.bm(10) do |r|
-  r.report 'fib_ruby(40)' do
-    a.fib_ruby(40)
-  end
-  r.report 'fib_wasm(40)' do
-    a.fib32(40)
-  end
+  r.report('ruby') { ruby.fib(40) }
+  r.report('wasmtime') { wmtm.fib32(40) }
+  r.report('wasmer') { wmer.fib32(40) }
 end
