059222e976c0e1638733ee12fba1b7d7532e11c6
kojix2 <2xijok@gmail.com>
2022-12-18 13:09:05 +0900

    Make error message friendly

--- lib/cry/compiler.rb
+++ lib/cry/compiler.rb
@@ -1,3 +1,5 @@
+require 'open3'
+
 module Cry
   # Crystal compiler command wrapper class.
   # This class is used to compile Crystal code to WASM.
@@ -5,6 +7,8 @@ module Cry
   #  compiler = Cry::Compiler.new
   #  compiler.build_wasm('puts "Hello, World!"')
   class Compiler
+    class CompilerError < StandardError; end
+
     # Crystal compiler options
     Options = Struct.new(:input, :output, :release, :export, :target, :link_flags)
 
@@ -50,14 +54,13 @@ module Cry
       set_options(crystal_code, **opts)
 
       command = build_command
-      result = system(command)
+
+      _o, error, status = Open3.capture3(command)
       @input_tempfile&.close
 
-      unless result
-        warn '[cry-wasm] Failed to compile Crystal code to WASM'
-        warn "[cry-wasm] #{command}"
-        warn crystal_code
-        raise 'crystal build failed' unless result
+      unless status.success?
+        warn build_error_message(crystal_code, command, error)
+        raise CompilerError, 'Failed to compile Crystal code to WASM'
       end
 
       wasm_bytes = IO.read(options.output, mode: 'rb')
@@ -110,6 +113,45 @@ module Cry
     def build_command(options = @options)
       link_flags = "\"#{options.link_flags}" + options.export.map { |n| "--export #{n} " }.join + '"'
       "crystal build #{options.input} -o #{options.output} --target #{options.target} --link-flags=#{link_flags}"
+    end
+
+    private
+
+    def build_error_message(crystal_code, command, error)
+      return <<~ERROR if error.include?('unable to find library')
+        [cry-wasm] Failed to compile Crystal code to WASM
+        [cry-wasm] Build command: \e[32m#{command}\e[0m
+        [cry-wasm] CRYSTAL_LIBRARY_PATH: #{get_crystal_library_path}"
+        [cry-wasm] #{error}
+        [cry-wasm] Library not found. 
+        [cry-wasm] Please set CRYSTAL_LIBRARY_PATH to the path of the Crystal library for WASM.
+        [cry-wasm] Or `bundle exec rake vendor:wasi_libs` to download the library.
+      ERROR
+
+      left_margin = crystal_code.split("\n").length.to_s.length
+      matches = error.scan(/:(\d+):(\d+)/)
+      line_number = matches[0][0].to_i if matches.any?
+      column_number = matches[0][1].to_i if matches.any?
+
+      <<~ERROR
+        [cry-wasm] Failed to compile Crystal code to WASM
+        [cry-wasm] Build command: \e[32m#{command}\e[0m
+        [cry-wasm] CRYSTAL_LIBRARY_PATH: #{get_crystal_library_path}"
+        #{crystal_code.split("\n").map.with_index do |line, i|
+          if i == line_number - 1
+            "[cry-wasm] \e[33m#{(i + 1).to_s.rjust(left_margin)}\e[0m #{line}\n" +
+            "[cry-wasm] #{' ' * (column_number + left_margin)}\e[1m\e[32m^\e[0m"
+          else
+            "[cry-wasm] \e[33m#{(i + 1).to_s.rjust(left_margin)}\e[0m #{line}"
+          end # {' '}
+        end.join("\n")}
+        [cry-wasm] Error: #{error}
+      ERROR
+    rescue StandardError
+      <<~ERROR
+        [cry-wasm] Failed to compile Crystal code to WASM
+        [cry-wasm] #{error}
+      ERROR
     end
   end
 end
