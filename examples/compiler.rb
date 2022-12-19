6e2d784fa0ebbec6a416da23bf701160c738b3d8
kojix2 <2xijok@gmail.com>
2022-12-19 09:41:10 +0900

    Improved example

--- examples/compiler.rb
+++ examples/compiler.rb
@@ -1,14 +1,22 @@
 require 'cry/wasm'
 
 compiler = Cry::Compiler.new
-
 wasm_bytes = compiler.build_wasm <<~CRYSTAL
-  p Crystal::DESCRIPTION
-  p Crystal::LIBRARY_PATH
-  p Crystal::LLVM_VERSION
-  p Crystal::PATH
-  p Crystal::VERSION
-  puts "Hello, Crystal World!"
+  class Hello
+    def initialize
+      @message = "Hello, Crystal WASM World!"
+      @info = [Crystal::DESCRIPTION,
+               Crystal::LIBRARY_PATH,
+               Crystal::PATH,
+               Crystal::VERSION].join("\n")
+    end
+
+    def hi
+      puts @message, @info
+    end
+  end
+
+  Hello.new.hi
 CRYSTAL
 
 Cry::Wasmer.new(wasm_bytes).start
